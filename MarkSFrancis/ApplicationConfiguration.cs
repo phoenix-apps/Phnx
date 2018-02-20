ab20c2626b79b17332b4a81210d675f2bacc54f2
Mark Francis <mrkfrancis8@gmail.com>
2018-02-20 22:09:15 +0000

    Added ApplicationConfiguration to allow reading from the application config

---
+++ MarkSFrancis/ApplicationConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Specialized;
+using System.Configuration;
+
+namespace MarkSFrancis
+{
+    public class ApplicationConfiguration
+    {
+        private NameValueCollection AppSettings { get; }
+
+        public ApplicationConfiguration(NameValueCollection appSettings)
+        {
+            AppSettings = appSettings;
+        }
+
+        public static ApplicationConfiguration FromConfig() => 
+            new ApplicationConfiguration(ConfigurationManager.AppSettings);
+
+        public string Get(string key)
+        {
+            return AppSettings[key];
+        }
+    }
+}
