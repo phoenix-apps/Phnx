19ad290be8c45ecd6f4f1e1c55b4d5b2aa28e802
Mark Francis <mrkfrancis8@gmail.com>
2018-02-23 16:39:56 +0000

    Added XML comments to Lazy classes

--- MarkSFrancis.Data/LazyLoad/Lazy.cs
+++ MarkSFrancis.Data/LazyLoad/Lazy.cs
@@ -2,40 +2,89 @@
 
 namespace MarkSFrancis.Data.LazyLoad
 {
+    /// <summary>
+    /// Lazy loading for a given property, with the option to set and get its value
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public class Lazy<T>
     {
         private readonly Func<T> _getFunc;
         private readonly Action<T> _setFunc;
 
+        /// <summary>
+        /// Occurs when the value for this item is set
+        /// </summary>
+        /// <param name="sender">The <see cref="Lazy{T}"/> that triggered the event</param>
+        /// <param name="newValue">The new value that has been assigned to the sender</param>
         public delegate void ValueSetEvent(Lazy<T> sender, T newValue);
 
+        /// <summary>
+        /// Occurs when the value for this item is loaded from an external source into the cache
+        /// </summary>
+        /// <param name="sender">The <see cref="Lazy{T}"/> that triggered the event</param>
+        /// <param name="cachedValue">The value that has been loaded into the cache</param>
         public delegate void ValueCachedEvent(Lazy<T> sender, T cachedValue);
-
+        
+        /// <summary>
+        /// Occurs when the value for this item is requested
+        /// </summary>
+        /// <param name="sender">The <see cref="Lazy{T}"/> that triggered the event</param>
+        /// <param name="value">The value of the sender that has retrieved</param>
         public delegate void ValueGetEvent(Lazy<T> sender, T value);
 
+        /// <summary>
+        /// Occurs when the value for this item is set
+        /// </summary>
         public event ValueSetEvent ValueSet;
+
+        /// <summary>
+        /// Occurs when the value for this item is loaded from an external source into the cache
+        /// </summary>
         public event ValueCachedEvent ValueCached;
+
+        /// <summary>
+        /// Occurs when the value for this item is loaded from an external source, or from the cache
+        /// </summary>
         public event ValueGetEvent ValueGet;
 
         private T _cachedValue;
         private bool _valueIsCached;
 
+        /// <summary>
+        /// Whether the value for this item can be set
+        /// </summary>
         public bool CanSet => _setFunc != null;
 
         private bool _valueChangedInLife;
+
+        /// <summary>
+        /// Whether the value of this has ever been set
+        /// </summary>
         public bool ValueChangedInLife => _valueChangedInLife;
 
+        /// <summary>
+        /// Create a new read-only <see cref="Lazy{T}"/> using a given function to load the data from an external source
+        /// </summary>
+        /// <param name="getFunction">The function to load the data from an external source</param>
         public Lazy(Func<T> getFunction)
         {
             _getFunc = getFunction;
         }
 
+        /// <summary>
+        /// Create a new <see cref="Lazy{T}"/> using a given function to load the data from an external source, and a given function to update the external source. The external source is updated immediately when the <see cref="Value"/> is set
+        /// </summary>
+        /// <param name="getFunction">The function to load the data from an external source</param>
+        /// <param name="setFunction">The function to send the data to an external source</param>
         public Lazy(Func<T> getFunction, Action<T> setFunction)
         {
             _getFunc = getFunction;
             _setFunc = setFunction;
         }
 
+        /// <summary>
+        /// The lazy-loaded value
+        /// </summary>
         public T Value
         {
             get
@@ -67,6 +116,9 @@ namespace MarkSFrancis.Data.LazyLoad
             }
         }
 
+        /// <summary>
+        /// Reset and clear the cache. The next time <see cref="Value"/> is retrieved, it will reload from the external source
+        /// </summary>
         public void ClearCache()
         {
             _valueIsCached = false;
--- MarkSFrancis.Data/LazyLoad/LazyDatabase.cs
+++ MarkSFrancis.Data/LazyLoad/LazyDatabase.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 
 namespace MarkSFrancis.Data.LazyLoad
 {
+    /// <summary>
+    /// A three-dimensional cache that can store up to an entire database including all its tables using each type and key to get each item from the cache
+    /// </summary>
     public static class LazyDatabase
     {
         private static Dictionary<Type, Dictionary<object, object>> CacheManager { get; set; }
@@ -10,6 +13,9 @@ namespace MarkSFrancis.Data.LazyLoad
 
         private static readonly object CacheLock = new object();
 
+        /// <summary>
+        /// The total number of types stored in the cache
+        /// </summary>
         public static int TotalTypesCached
         {
             get
@@ -21,6 +27,9 @@ namespace MarkSFrancis.Data.LazyLoad
             }
         }
 
+        /// <summary>
+        /// The total number of records across all types stored in the cache
+        /// </summary>
         public static int TotalItemsCached
         {
             get
@@ -39,6 +48,14 @@ namespace MarkSFrancis.Data.LazyLoad
 
         private static bool CacheIsMaxSize { get; set; }
         
+        /// <summary>
+        /// Get a given value from the cache
+        /// </summary>
+        /// <typeparam name="TKey">The type of the unique identifier for this item</typeparam>
+        /// <typeparam name="TObject">The type of object to load from the cache</typeparam>
+        /// <param name="id">The unique identifier for this item</param>
+        /// <param name="getFunc">The method to call if the given item is not already in the cache</param>
+        /// <returns></returns>
         public static TObject GetValue<TKey, TObject>(TKey id, Func<TKey, TObject> getFunc)
         {
             lock (CacheLock)
@@ -53,6 +70,11 @@ namespace MarkSFrancis.Data.LazyLoad
             }
         }
 
+        /// <summary>
+        /// Gets the total number of items cached for this given type
+        /// </summary>
+        /// <param name="type">The type to get the number of records cached for</param>
+        /// <returns></returns>
         public static int TotalItemsCachedForType(Type type)
         {
             lock (CacheLock)
@@ -61,6 +83,9 @@ namespace MarkSFrancis.Data.LazyLoad
             }
         }
 
+        /// <summary>
+        /// Clear all items from the cache, forcing all new requests to be reloaded into the cache
+        /// </summary>
         public static void ClearAllCache()
         {
             lock (CacheLock)
@@ -70,6 +95,10 @@ namespace MarkSFrancis.Data.LazyLoad
             }
         }
 
+        /// <summary>
+        /// Clear all items from the cache for a given type, forcing all new requests for that type to be reloaded into the cache
+        /// </summary>
+        /// <param name="typeToClear"></param>
         public static void ClearAllCache(Type typeToClear)
         {
             lock (CacheLock)
