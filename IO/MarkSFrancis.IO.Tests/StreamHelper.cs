ba3c39baad4308fd8f87a3ae4a584d0a65e19b2f
Mark Francis <mrkfrancis8@gmail.com>
2018-02-23 20:28:52 +0000

    Added Tests helper for IO

---
+++ IO/MarkSFrancis.IO.Tests/StreamHelper.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using System.Text;
+
+namespace MarkSFrancis.IO.Tests
+{
+    public static class StreamHelper
+    {
+        public static MemoryStream ToStream(this string s)
+        {
+            byte[] byteArray = Encoding.UTF8.GetBytes(s);
+
+            return new MemoryStream(byteArray);
+        }
+
+        public static StreamWriter CreateStreamWriter(out MemoryStream ms)
+        {
+            ms = new MemoryStream();
+            return new StreamWriter(ms);
+        }
+
+        public static string ReadStream(Stream s)
+        {
+            s.Seek(0, SeekOrigin.Begin);
+            StreamReader reader = new StreamReader(s);
+            return reader.ReadToEnd();
+        }
+    }
+}
