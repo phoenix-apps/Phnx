7d8d6a26c46f8df38ba55e6abefbe43ec42c523f
Mark Francis <mrkfrancis8@gmail.com>
2018-03-04 12:45:45 +0000

    Moved threads demo into its own class

--- SyntaxTest/Program.cs
+++ SyntaxTest/Program.cs
@@ -1,44 +1,22 @@
-﻿using System.Threading;
-using System.Threading.Tasks;
-using MarkSFrancis.Console;
+﻿using MarkSFrancis.Console;
 
 namespace SyntaxTest
 {
     class Program
     {
         public static readonly ConsoleIo Console = new ConsoleIo();
-        // private static object SyncContext = new object();
 
         static void Main()
         {
-            int val = 0, incCount = 100000000;
-            Task thd1 = new Task(() => IncrementLots(ref val, incCount / 2));
-            Task thd2 = new Task(() => IncrementLots(ref val, incCount / 2));
-            thd1.Start();
-            thd2.Start();
 
-            Task.WaitAll(thd1, thd2);
-
-            Console.WriteLine("Value of val: ");
-            Console.WriteLine(val);
-            Console.WriteLine();
-
-            Console.WriteLine("Value should be: ");
-            Console.WriteLine(incCount);
 
             Console.ReadKey();
         }
 
-        private static void IncrementLots(ref int value, int timesToIncrement)
+        static void RunThreadsDemo()
         {
-            for (int incrementCount = 0; incrementCount < timesToIncrement; ++incrementCount)
-            {
-                // Interlocked.Increment(ref value);
-                // lock (SyncContext)
-                // {
-                ++value;
-                // }
-            }
+            ThreadsDemo threads = new ThreadsDemo(Console);
+            threads.Run();
         }
     }
 }
---
+++ SyntaxTest/TheadsDemo.cs
@@ -0,0 +1,47 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MarkSFrancis.Console;
+
+namespace SyntaxTest
+{
+    class ThreadsDemo
+    {
+        public ThreadsDemo(ConsoleIo console)
+        {
+            _console = console;
+        }
+
+        private readonly ConsoleIo _console;
+        // private object _syncContext = new object();
+
+        public void Run()
+        {
+            int val = 0, incCount = 100000000;
+            Task thd1 = new Task(() => IncrementLots(ref val, incCount / 2));
+            Task thd2 = new Task(() => IncrementLots(ref val, incCount / 2));
+            thd1.Start();
+            thd2.Start();
+
+            Task.WaitAll(thd1, thd2);
+
+            _console.WriteLine("Value of val: ");
+            _console.WriteLine(val);
+            _console.WriteLine();
+
+            _console.WriteLine("Value should be: ");
+            _console.WriteLine(incCount);
+        }
+
+        private void IncrementLots(ref int value, int timesToIncrement)
+        {
+            for (int incrementCount = 0; incrementCount < timesToIncrement; ++incrementCount)
+            {
+                // Interlocked.Increment(ref value);
+                // lock (_syncContext)
+                // {
+                ++value;
+                // }
+            }
+        }
+    }
+}
