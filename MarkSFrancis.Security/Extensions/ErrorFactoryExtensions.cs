f9e81db39dbb6c423cb1040b7831482fa30e1fe5
Mark Francis <mrkfrancis8@gmail.com>
2018-03-06 21:18:44 +0000

    Added error messages to prevent misconfigured hash generators in future

--- MarkSFrancis.Security/Extensions/ErrorFactoryExtensions.cs
+++ MarkSFrancis.Security/Extensions/ErrorFactoryExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using MarkSFrancis.Security.Passwords.Interface;
 
 namespace MarkSFrancis.Security.Extensions
 {
@@ -18,6 +19,19 @@ namespace MarkSFrancis.Security.Extensions
             int saltLengthWas)
         {
             return new ArgumentException($"The salt was an invalid length. The salt length should be {saltLengthShouldBe} but was {saltLengthWas}");
+        }
+        
+        /// <summary>
+        /// An error to describe that the salt length is invalid
+        /// </summary>
+        /// <param name="factory">The factory to extend</param>
+        /// <param name="hashLength">The length that the hash actually was</param>
+        /// <param name="hashLengthShouldBe">The length that the hash should've been</param>
+        /// <param name="generatorVersion">The hash generator version for the <see cref="IPasswordHashVersion"/> used</param>
+        /// <returns>A <see cref="TypeLoadException"/> asking the user to check the <see cref="IPasswordHashVersion"/> and the hash</returns>
+        public static TypeLoadException InvalidHashConfiguration(this ErrorFactory factory, int hashLength, int hashLengthShouldBe, int generatorVersion)
+        {
+            return new TypeLoadException($"The configuration for hash generator version {generatorVersion} does not match with the hash. The hash length should be {hashLengthShouldBe} but was {hashLength}. Please check that this hash belongs to the assigned {nameof(IPasswordHashVersion)}, and that the {nameof(IPasswordHashVersion.SaltBytesLength)} and {nameof(IPasswordHashVersion.HashBytesLength)} are correct");
         }
     }
 }
--- MarkSFrancis.Security/Passwords/VersionedHash.cs
+++ MarkSFrancis.Security/Passwords/VersionedHash.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Text;
+using MarkSFrancis.Security.Extensions;
 using MarkSFrancis.Security.Passwords.Interface;
 
 namespace MarkSFrancis.Security.Passwords
@@ -7,6 +8,8 @@ namespace MarkSFrancis.Security.Passwords
     internal class VersionedHash
     {
         private static readonly Encoding DefaultPasswordEncoding = Encoding.Unicode;
+
+        public const int BytesUsedByVersionTag = 4;
         
         public VersionedHash(string password, IPasswordHashVersion hashGenerator)
         {
@@ -15,6 +18,8 @@ namespace MarkSFrancis.Security.Passwords
 
             var passBytes = DefaultPasswordEncoding.GetBytes(password);
             PasswordHash = hashGenerator.GenerateHash(passBytes, Salt);
+
+            VerifyGenerator(hashGenerator);
         }
 
         public VersionedHash(string password, byte[] salt, IPasswordHashVersion hashGenerator)
@@ -24,17 +29,42 @@ namespace MarkSFrancis.Security.Passwords
 
             var passBytes = DefaultPasswordEncoding.GetBytes(password);
             PasswordHash = hashGenerator.GenerateHash(passBytes, salt);
+
+            VerifyGenerator(hashGenerator);
         }
 
         public VersionedHash(byte[] bytes, IPasswordHashVersion hashGenerator)
         {
+            VerifyGenerator(bytes.Length, hashGenerator);
+
             Version = BitConverter.ToInt32(bytes, 0);
 
             PasswordHash = new byte[hashGenerator.HashBytesLength];
-            Array.Copy(bytes, 4, PasswordHash, 0, PasswordHash.Length);
+            Array.Copy(bytes, BytesUsedByVersionTag, PasswordHash, 0, PasswordHash.Length);
 
             Salt = new byte[hashGenerator.SaltBytesLength];
-            Array.Copy(bytes, 4 + PasswordHash.Length, Salt, 0, Salt.Length);
+            Array.Copy(bytes, BytesUsedByVersionTag + PasswordHash.Length, Salt, 0, Salt.Length);
+        }
+
+        private void VerifyGenerator(IPasswordHashVersion generator)
+        {
+            var hashLength = BytesUsedByVersionTag + PasswordHash.Length + Salt.Length;
+            var hashLengthShouldBe = BytesUsedByVersionTag + generator.HashBytesLength + generator.SaltBytesLength;
+
+
+            if (hashLength != BytesUsedByVersionTag + generator.HashBytesLength + generator.SaltBytesLength)
+            {
+                throw ErrorFactory.Default.InvalidHashConfiguration(hashLength, hashLengthShouldBe, generator.Version);
+            }
+        }
+
+        private void VerifyGenerator(int hashLength, IPasswordHashVersion generator)
+        {
+            var hashLengthShouldBe = BytesUsedByVersionTag + generator.HashBytesLength + generator.SaltBytesLength;
+            if (hashLength != BytesUsedByVersionTag + generator.HashBytesLength + generator.SaltBytesLength)
+            {
+                throw ErrorFactory.Default.InvalidHashConfiguration(hashLength, hashLengthShouldBe, generator.Version);
+            }
         }
 
         public static int GetVersionFromBytes(byte[] bytes)
@@ -50,15 +80,15 @@ namespace MarkSFrancis.Security.Passwords
 
         public byte[] ToBytes()
         {
-            byte[] returnBytes = new byte[4 + PasswordHash.Length + Salt.Length];
+            byte[] returnBytes = new byte[BytesUsedByVersionTag + PasswordHash.Length + Salt.Length];
 
             byte[] versionBytes = BitConverter.GetBytes(Version);
 
-            Array.Copy(versionBytes, returnBytes, 4);
+            Array.Copy(versionBytes, returnBytes, BytesUsedByVersionTag);
 
-            Array.Copy(PasswordHash, 0, returnBytes, 4, PasswordHash.Length);
+            Array.Copy(PasswordHash, 0, returnBytes, BytesUsedByVersionTag, PasswordHash.Length);
 
-            Array.Copy(Salt, 0, returnBytes, 4 + PasswordHash.Length, Salt.Length);
+            Array.Copy(Salt, 0, returnBytes, BytesUsedByVersionTag + PasswordHash.Length, Salt.Length);
 
             return returnBytes;
         }
