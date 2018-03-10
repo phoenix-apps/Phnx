c79cdf9f716994bbc2a2beb81cb1c746b50d6db6
Mark Francis <mrkfrancis8@gmail.com>
2018-03-10 15:04:48 +0000

    Removed DelimitedData project as CsvHelper offers all the same features, and more

--- IO/MarkSFrancis.IO.DelimitedData/Maps/Interfaces/IMapBy.cs
+++
@@ -1,9 +0,0 @@
-﻿using MarkSFrancis.Reflection;
-
-namespace MarkSFrancis.IO.DelimitedData.Maps.Interfaces
-{
-    public interface IMapBy<T, TMapBy> : IMap<T> where T : new()
-    {
-        void Map(PropertyFieldInfo<T, object> propFieldToMap, TMapBy mapTo);
-    }
-}
