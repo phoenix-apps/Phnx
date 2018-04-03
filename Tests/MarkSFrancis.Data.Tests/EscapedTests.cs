03f517f28b250a231fa89ead0482828df944fc78
Mark Francis <mrkfrancis8@gmail.com>
2018-04-03 17:02:50 +0100

    Added Escaped tests for Data

---
+++ Tests/MarkSFrancis.Data.Tests/EscapedTests.cs
@@ -0,0 +1,57 @@
+﻿using NUnit.Framework;
+
+namespace MarkSFrancis.Data.Tests
+{
+    public class EscapedTests
+    {
+        [Test]
+        public void EscapingText_WithNoContent_ReturnsEmptyString()
+        {
+            string text = string.Empty;
+
+            string result = Escaped.Escape(text, '\0', '\0');
+
+            Assert.AreEqual(text, result);
+        }
+
+        [Test]
+        public void EscapingText_WithContentAndNoEscapeChars_ReturnsContent()
+        {
+            string text = "asdf";
+
+            string result = Escaped.Escape(text, '\0', '\0');
+
+            Assert.AreEqual(text, result);
+        }
+
+        [Test]
+        public void EscapingText_WithEscapeChar_ReturnsEscaped()
+        {
+            string text = "as\0df";
+
+            string result = Escaped.Escape(text, '\0');
+
+            Assert.AreEqual("as\0\0df", result);
+        }
+
+        [Test]
+        public void EscapingText_WithCharsToEscape_ReturnsEscaped()
+        {
+            string text = "asdf";
+
+            string result = Escaped.Escape(text, '\0', 'a');
+
+            Assert.AreEqual("\0asdf", result);
+        }
+
+        [Test]
+        public void EscapingText_WithEscapeCharAndCharsToEscape_ReturnsEscaped()
+        {
+            string text = "as\0df";
+
+            string result = Escaped.Escape(text, '\0', 'a');
+
+            Assert.AreEqual("\0as\0\0df", result);
+        }
+    }
+}
