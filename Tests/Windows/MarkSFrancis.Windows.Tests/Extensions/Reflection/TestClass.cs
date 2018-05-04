39b8424f405050045a2c8cc62250e82d53a7d5ac
Mark Francis <mrkfrancis8@gmail.com>
2018-05-04 15:39:53 +0100

    Added tests for Windows project

---
+++ Tests/Windows/MarkSFrancis.Windows.Tests/Extensions/Reflection/TestClass.cs
@@ -0,0 +1,15 @@
+﻿namespace MarkSFrancis.Windows.Tests.Extensions.Reflection
+{
+    public class TestClass
+    {
+        public string AutoProperty { get; set; }
+
+        public string NotAutoProperty
+        {
+            get => Field;
+            set => Field = value;
+        }
+
+        public string Field = "asdf";
+    }
+}
