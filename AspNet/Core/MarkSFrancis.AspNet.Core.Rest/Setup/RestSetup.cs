c2161226f2d61afe3e1d0eff9f460da808c56c0c
Mark Francis <mrkfrancis8@gmail.com>
2018-06-07 17:08:07 +0100

    Added helpers for setting up dependancy injection

---
+++ AspNet/Core/MarkSFrancis.AspNet.Core.Rest/Setup/RestSetup.cs
@@ -0,0 +1,42 @@
+﻿using MarkSFrancis.AspNet.Core.Rest.Services;
+using MarkSFrancis.AspNet.Core.Rest.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MarkSFrancis.AspNet.Core.Rest.Setup
+{
+    /// <summary>
+    /// A service builder specifically for setting up <see cref="Rest"/> for injection
+    /// </summary>
+    internal class RestServiceBuilder
+    {
+        /// <summary>
+        /// The service collection for dependancy injection
+        /// </summary>
+        private readonly IServiceCollection services;
+
+        internal RestServiceBuilder(IServiceCollection services)
+        {
+            this.services = services;
+        }
+
+        public void RegisterETags()
+        {
+            services.AddScoped<IETagService, ETagService>();
+        }
+
+        public void RegisterContextHelpers()
+        {
+            services.AddSingleton<IActionContextAccessor, ActionContextAccessor>();
+        }
+
+        public void RegisterRestHelpers()
+        {
+            services.AddScoped(typeof(IRestRequestService<>), typeof(RestRequestService<>));
+
+            services.AddScoped(typeof(IRestResponseService<,,>), typeof(RestResponseService<,,>));
+
+            services.AddScoped(typeof(IRestControllerHelperService<,,,>), typeof(RestControllerHelperService<,,,>));
+        }
+    }
+}
