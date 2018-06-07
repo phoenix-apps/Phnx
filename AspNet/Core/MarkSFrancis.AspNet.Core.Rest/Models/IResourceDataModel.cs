5a47b0a5b4f7b87f506e0bb829ab27db22a55a35
Mark Francis <mrkfrancis8@gmail.com>
2018-06-07 16:52:57 +0100

    Added REST API Helpers for Core

---
+++ AspNet/Core/MarkSFrancis.AspNet.Core.Rest/Models/IResourceDataModel.cs
@@ -0,0 +1,14 @@
+﻿namespace MarkSFrancis.AspNet.Core.Rest.Models
+{
+    /// <summary>
+    /// A data model that can be used with e-tags
+    /// </summary>
+    public interface IResourceDataModel
+    {
+        /// <summary>
+        /// The version number of this data model. 
+        /// This should be incremented each time the data is edited
+        /// </summary>
+        byte[] RowVersion { get; }
+    }
+}
