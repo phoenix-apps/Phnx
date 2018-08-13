9529c1b417e83ad2a3e90ac295feca2e05bcf990
Mark Francis <mrkfrancis8@gmail.com>
2018-08-13 17:00:13 +0100

    Added tests for AsynExtensions

---
+++ Tests/MarkSFrancis.Tests/Extensions/AsyncExtensionsTests.cs
@@ -0,0 +1,59 @@
+﻿using MarkSFrancis.Extensions;
+using NUnit.Framework;
+using System;
+
+namespace MarkSFrancis.Tests.Extensions
+{
+    public class AsyncExtensionsTests
+    {
+        [Test]
+        public void StartingAsyncMethod_WithoutContext_RunsMethod()
+        {
+            int? result = null;
+            Action methodStub = () => result = 0;
+
+            var task = methodStub.InvokeAsync(false);
+
+            task.Wait();
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void StartingAsyncMethod_WithContext_RunsMethod()
+        {
+            int? result = null;
+            Action methodStub = () => result = 0;
+
+            var task = methodStub.InvokeAsync(true);
+
+            task.Wait();
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void StartingAsyncFunc_WithoutContext_RunsMethod()
+        {
+            int? result = null;
+            Func<int?> methodStub = () => 0;
+
+            var task = methodStub.InvokeAsync(false);
+
+            task.Wait();
+            result = task.Result;
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void StartingAsyncFunc_WithContext_RunsMethod()
+        {
+            int? result = null;
+            Func<int?> methodStub = () => 0;
+
+            var task = methodStub.InvokeAsync(true);
+
+            task.Wait();
+            result = task.Result;
+            Assert.AreEqual(0, result);
+        }
+    }
+}
