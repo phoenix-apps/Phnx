d7268639db9b9c75d0eba453f15d46b0db2b405f
Mark Francis <mrkfrancis8@gmail.com>
2018-08-14 13:08:08 +0100

    Created tests for CreateInstance extension (TypeExtensions)

---
+++ Tests/MarkSFrancis.Tests/Fakes/TypeFakes/TypeWithBrokenConstructor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MarkSFrancis.Tests.Fakes.TypeFakes
+{
+    public class TypeWithBrokenConstructor
+    {
+        public TypeWithBrokenConstructor()
+        {
+            throw new Exception();
+        }
+    }
+}
