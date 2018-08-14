519b82d27cadee5a7745ad9748df5c27be8e8d46
Mark Francis <mrkfrancis8@gmail.com>
2018-08-14 14:25:11 +0100

    Added long extensions tests

--- Tests/MarkSFrancis.Tests/Extensions/Numeric/LongExtensionsTests.cs
+++ Tests/MarkSFrancis.Tests/Extensions/Numeric/LongExtensionsTests.cs
@@ -3,32 +3,32 @@ using NUnit.Framework;
 
 namespace MarkSFrancis.Tests.Extensions.Numeric
 {
-    public class IntExtensionsTests
+    public class LongExtensionsTests
     {
         [Test]
-        public void RoundToNearest7_WhenValueIs2147483646_ReturnsValue()
+        public void RoundToNearest7_WhenValueIs9223372036854775807_ReturnsValue()
         {
-            int value = 2147483646;
+            long value = 9223372036854775807;
 
             var result = value.RoundToNearest(7);
 
-            Assert.AreEqual(2147483646, result);
+            Assert.AreEqual(value, result);
         }
 
         [Test]
-        public void RoundToNearest7_WhenValueIs2147483647_Returns2147483646()
+        public void RoundToNearest7_WhenValueIs9223372036854775802_Returns9223372036854775800()
         {
-            int value = 2147483647;
+            long value = 9223372036854775802;
 
             var result = value.RoundToNearest(7);
 
-            Assert.AreEqual(2147483646, result);
+            Assert.AreEqual(9223372036854775800, result);
         }
 
         [Test]
         public void RoundToNearest6_WhenValueIs9_Returns12()
         {
-            int value = 9;
+            long value = 9;
 
             var result = value.RoundToNearest(6);
 
@@ -38,7 +38,7 @@ namespace MarkSFrancis.Tests.Extensions.Numeric
         [Test]
         public void RoundToNearest6_WhenValueIs0_Returns0()
         {
-            int value = 0;
+            long value = 0;
 
             var result = value.RoundToNearest(6);
 
@@ -48,7 +48,7 @@ namespace MarkSFrancis.Tests.Extensions.Numeric
         [Test]
         public void Converting0ToString_With0MinimumDecimals_Returns0()
         {
-            var result = 0.ToString(0);
+            var result = 0L.ToString(0);
 
             Assert.AreEqual("0", result);
         }
@@ -56,7 +56,7 @@ namespace MarkSFrancis.Tests.Extensions.Numeric
         [Test]
         public void Converting0ToString_With5MinimumDecimals_Returns00000()
         {
-            var result = 0.ToString(5);
+            var result = 0L.ToString(5);
 
             Assert.AreEqual("00000", result);
         }
@@ -64,7 +64,7 @@ namespace MarkSFrancis.Tests.Extensions.Numeric
         [Test]
         public void Converting100ToString_With0MinimumDecimals_Returns100()
         {
-            var result = 100.ToString(0);
+            var result = 100L.ToString(0);
 
             Assert.AreEqual("100", result);
         }
@@ -72,7 +72,7 @@ namespace MarkSFrancis.Tests.Extensions.Numeric
         [Test]
         public void Converting100ToString_With5MinimumDecimals_Returns00100()
         {
-            var result = (100).ToString(5);
+            var result = (100L).ToString(5);
 
             Assert.AreEqual("00100", result);
         }
@@ -80,7 +80,7 @@ namespace MarkSFrancis.Tests.Extensions.Numeric
         [Test]
         public void ConvertingMinus100ToString_With0MinimumDecimals_ReturnsMinus100()
         {
-            var result = (-100).ToString(0);
+            var result = (-100L).ToString(0);
 
             Assert.AreEqual("-100", result);
         }
@@ -88,7 +88,7 @@ namespace MarkSFrancis.Tests.Extensions.Numeric
         [Test]
         public void ConvertingMinus100ToString_With5MinimumDecimals_ReturnsMinus00100()
         {
-            var result = (-100).ToString(5);
+            var result = (-100L).ToString(5);
 
             Assert.AreEqual("-00100", result);
         }
