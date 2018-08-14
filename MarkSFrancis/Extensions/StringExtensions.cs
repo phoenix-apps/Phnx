e381ad7bbdf1204474a8af4fdd5084c7fd1c2185
Mark Francis <mrkfrancis8@gmail.com>
2018-08-14 11:45:34 +0100

    Now uses ArgumentNullException for StringExtensions, improved performance for Remove, and fixed a bug where the Remove was sensitive to the order of the strings to remove

--- MarkSFrancis/Extensions/StringExtensions.cs
+++ MarkSFrancis/Extensions/StringExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace MarkSFrancis.Extensions
@@ -9,13 +10,12 @@ namespace MarkSFrancis.Extensions
     /// </summary>
     public static class StringExtensions
     {
-
         /// <summary>Concatenates the members of a collection, using the specified separator between each member</summary>
         /// <param name="separator">The string to use as a separator. The separator is only included in the returned string if <paramref name="values"/> has more than one element</param>
         /// <param name="values">A collection that contains the objects to concatenate</param>
         /// <typeparam name="T">The type of the members of <paramref name="values"/></typeparam>
         /// <returns>A string that consists of the members of <paramref name="values"/> delimited by the <paramref name="separator">separator</paramref> string. If <paramref name="values"/> has no members, the method returns <see cref="String.Empty"></see></returns>
-        /// <exception cref="System.ArgumentNullException"><paramref name="values"/> is null.</exception>
+        /// <exception cref="ArgumentNullException"><paramref name="values"/> is null.</exception>
         public static string Join<T>(this IEnumerable<T> values, string separator)
         {
             return string.Join(separator, values);
@@ -24,83 +24,116 @@ namespace MarkSFrancis.Extensions
         /// <summary>
         /// Get if this string is <see langword="null"/>, <see cref="string.Empty"/>, or whitespace
         /// </summary>
-        /// <param name="str">The string to check</param>
+        /// <param name="stringToTest">The string to check</param>
         /// <returns></returns>
-        public static bool IsEmptyOrWhitespace(this string str)
+        public static bool IsNullOrEmptyOrWhitespace(this string stringToTest)
         {
-            return string.IsNullOrWhiteSpace(str);
+            return string.IsNullOrWhiteSpace(stringToTest);
         }
 
         /// <summary>
-        /// Get if this string is <see langword="null"/>, <see cref="string.Empty"/>
+        /// Get if this string is <see langword="null"/> or <see cref="string.Empty"/>
         /// </summary>
-        /// <param name="str">The string to check</param>
+        /// <param name="stringToTest">The string to check</param>
         /// <returns></returns>
-        public static bool IsNullOrEmpty(this string str)
+        public static bool IsNullOrEmpty(this string stringToTest)
         {
-            return string.IsNullOrEmpty(str);
-        }
-
-        /// <summary>
-        /// Removes a series of characters from a string
-        /// </summary>
-        /// <param name="s">The string to remove the characters from</param>
-        /// <param name="charsToRemove">The characters to remove</param>
-        /// <returns></returns>
-        public static string Remove(this string s, params char[] charsToRemove)
-        {
-            return Remove(s, (IEnumerable<char>)charsToRemove);
+            return string.IsNullOrEmpty(stringToTest);
         }
 
         /// <summary>
         /// Removes a series of characters from a string
         /// </summary>
-        /// <param name="s">The string to remove the characters from</param>
+        /// <param name="stringToRemoveFrom">The string to remove the characters from</param>
         /// <param name="charsToRemove">The characters to remove</param>
         /// <returns></returns>
-        public static string Remove(this string s, IEnumerable<char> charsToRemove)
+        public static string Remove(this string stringToRemoveFrom, params char[] charsToRemove)
         {
-            string retString = s;
-            foreach (var c in charsToRemove)
+            return Remove(stringToRemoveFrom, (IEnumerable<char>)charsToRemove);
+        }
+
+        /// <summary>
+        /// Removes a series of characters from a string
+        /// </summary>
+        /// <param name="stringToRemoveFrom">The string to remove the characters from</param>
+        /// <param name="charsToRemove">The characters to remove</param>
+        /// <returns></returns>
+        public static string Remove(this string stringToRemoveFrom, IEnumerable<char> charsToRemove)
+        {
+            if (stringToRemoveFrom == null)
             {
-                retString = retString.Replace(c.ToString(), string.Empty);
+                throw ErrorFactory.Default.ArgumentNull(nameof(stringToRemoveFrom));
             }
 
-            return retString;
+            if (charsToRemove == null)
+            {
+                throw ErrorFactory.Default.ArgumentNull(nameof(charsToRemove));
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            var charsToRemoveHash = new HashSet<char>(charsToRemove);
+
+            foreach (char c in stringToRemoveFrom)
+            {
+                if (!charsToRemoveHash.Contains(c))
+                {
+                    sb.Append(c);
+                }
+            }
+
+            return sb.ToString();
         }
 
         /// <summary>
         /// Removes a string from another string
         /// </summary>
-        /// <param name="s">The string to remove from</param>
+        /// <param name="stringToRemoveFrom">The string to remove from</param>
         /// <param name="textToRemove">The string to remove</param>
         /// <returns></returns>
-        public static string Remove(this string s, string textToRemove)
+        public static string Remove(this string stringToRemoveFrom, string textToRemove)
         {
-            return s.Replace(textToRemove, string.Empty);
+            if (stringToRemoveFrom == null)
+            {
+                throw ErrorFactory.Default.ArgumentNull(nameof(stringToRemoveFrom));
+            }
+
+            return stringToRemoveFrom.Replace(textToRemove, string.Empty);
         }
 
         /// <summary>
         /// Removes a series of strings from a string
         /// </summary>
-        /// <param name="s">The string to remove the strings from</param>
+        /// <param name="stringToRemoveFrom">The string to remove the strings from</param>
         /// <param name="textToRemove">The strings to remove</param>
         /// <returns></returns>
-        public static string Remove(this string s, params string[] textToRemove)
+        public static string Remove(this string stringToRemoveFrom, params string[] textToRemove)
         {
-            return Remove(s, (IEnumerable<string>)textToRemove);
+            return Remove(stringToRemoveFrom, (IEnumerable<string>)textToRemove);
         }
 
         /// <summary>
         /// Removes a series of strings from a string
         /// </summary>
-        /// <param name="s">The string to remove the strings from</param>
+        /// <param name="stringToRemoveFrom">The string to remove the strings from</param>
         /// <param name="textToRemove">The strings to remove</param>
         /// <returns></returns>
-        public static string Remove(this string s, IEnumerable<string> textToRemove)
+        public static string Remove(this string stringToRemoveFrom, IEnumerable<string> textToRemove)
         {
-            string retString = s;
-            foreach (var text in textToRemove)
+            if (stringToRemoveFrom == null)
+            {
+                throw ErrorFactory.Default.ArgumentNull(nameof(stringToRemoveFrom));
+            }
+
+            if (textToRemove == null)
+            {
+                throw ErrorFactory.Default.ArgumentNull(nameof(textToRemove));
+            }
+
+            string retString = stringToRemoveFrom;
+
+            // TODO Optimise. Currently (o)²
+            foreach (var text in textToRemove.OrderByDescending(t => t.Length))
             {
                 retString = retString.Replace(text, string.Empty);
             }
@@ -111,14 +144,19 @@ namespace MarkSFrancis.Extensions
         /// <summary>
         /// Converts a string to camel case
         /// </summary>
-        /// <param name="str">The string to convert</param>
+        /// <param name="stringToConvert">The string to convert</param>
         /// <param name="firstCharIsUpper">Whether the first character should be upper case. Setting this to true is the same as <see cref="ToPascalCase"/></param>
         /// <returns></returns>
-        public static string ToCamelCase(this string str, bool firstCharIsUpper)
+        public static string ToCamelCase(this string stringToConvert, bool firstCharIsUpper)
         {
+            if (stringToConvert == null)
+            {
+                throw ErrorFactory.Default.ArgumentNull(nameof(stringToConvert));
+            }
+
             var wordDelimiters = new[] { " ", "-", "_" };
 
-            var words = str.Split(wordDelimiters, StringSplitOptions.RemoveEmptyEntries);
+            var words = stringToConvert.Split(wordDelimiters, StringSplitOptions.RemoveEmptyEntries);
 
             if (words.Length == 0)
             {
@@ -135,7 +173,7 @@ namespace MarkSFrancis.Extensions
                 return char.ToUpper(word[0]) + word.Substring(1, word.Length - 1).ToLower();
             }
 
-            StringBuilder camelCase = new StringBuilder(str.Length);
+            StringBuilder camelCase = new StringBuilder(stringToConvert.Length);
 
             camelCase.Append(firstCharIsUpper ? MakeWordCamelCase(words[0]) : words[0].ToLower());
 
@@ -150,22 +188,27 @@ namespace MarkSFrancis.Extensions
         /// <summary>
         /// Converts a string to camel case
         /// </summary>
-        /// <param name="str">The string to convert</param>
+        /// <param name="stringToConvert">The string to convert</param>
         /// <returns></returns>
-        public static string ToPascalCase(this string str)
+        public static string ToPascalCase(this string stringToConvert)
         {
-            return ToCamelCase(str, true);
+            return ToCamelCase(stringToConvert, true);
         }
 
         /// <summary>
         /// Converts a camel case or pascal case formatted string to a normal string
         /// </summary>
-        /// <param name="str">The string to convert</param>
+        /// <param name="stringToConvert">The string to convert</param>
         /// <param name="startEachWordWithCapital">Whether to start each word with a capital letter, such as in a title</param>
         /// <returns></returns>
-        public static string FromCamelAndPascalCase(this string str, bool startEachWordWithCapital = false)
+        public static string FromCamelAndPascalCase(this string stringToConvert, bool startEachWordWithCapital = false)
         {
-            if (str.Length == 0)
+            if (stringToConvert == null)
+            {
+                throw ErrorFactory.Default.ArgumentNull(nameof(stringToConvert));
+            }
+
+            if (stringToConvert.Length == 0)
             {
                 return string.Empty;
             }
@@ -222,9 +265,9 @@ namespace MarkSFrancis.Extensions
                 }
             }
 
-            for (int index = 0; index < str.Length; index++)
+            for (int index = 0; index < stringToConvert.Length; index++)
             {
-                char curChar = str[index];
+                char curChar = stringToConvert[index];
 
                 if (char.IsNumber(curChar))
                 {
