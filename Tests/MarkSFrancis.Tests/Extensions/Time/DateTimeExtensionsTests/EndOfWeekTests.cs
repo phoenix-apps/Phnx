4257518bc2da916d8cf975c483ed27010ac47e6a
Mark Francis <mrkfrancis8@gmail.com>
2018-08-15 11:41:18 +0100

    Improved tests for StartOfWeek and added tests for EndOfWeek

---
+++ Tests/MarkSFrancis.Tests/Extensions/Time/DateTimeExtensionsTests/EndOfWeekTests.cs
@@ -0,0 +1,74 @@
+﻿using MarkSFrancis.Extensions.Time;
+using NUnit.Framework;
+using System;
+
+namespace MarkSFrancis.Tests.Extensions.Time.DateTimeExtensionsTests
+{
+    public class EndOfWeekTests
+    {
+        [Test]
+        public void EndOfWeek_WhenFirstDayIsSundayAndDateIsSunday_ReturnsEndOfNextSaturday()
+        {
+            DateTime testDate = new DateTime(2000, 1, 2);
+            DateTime expectedResult = new DateTime(2000, 1, 9).AddTicks(-1);
+
+            var result = testDate.EndOfWeek(false);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        public void StartOfWeek_WhenFirstDayIsSundayAndDateIsWednesday_ReturnsEndOfNextSaturday()
+        {
+            DateTime testDate = new DateTime(2000, 1, 19);
+            DateTime expectedResult = new DateTime(2000, 1, 23).AddTicks(-1);
+
+            var result = testDate.EndOfWeek(false);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+        [Test]
+        public void EndOfWeek_WhenFirstDayIsSundayAndDateIsSaturday_ReturnsEndOfSameDay()
+        {
+            DateTime testDate = new DateTime(2000, 1, 1);
+            DateTime expectedResult = new DateTime(2000, 1, 2).AddTicks(-1);
+
+            var result = testDate.EndOfWeek(false);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        public void EndOfWeek_WhenFirstDayIsMondayAndDateIsMonday_ReturnsEndOfNextSunday()
+        {
+            DateTime testDate = new DateTime(2000, 1, 3);
+            DateTime expectedResult = new DateTime(2000, 1, 10).AddTicks(-1);
+
+            var result = testDate.EndOfWeek();
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        public void EndOfWeek_WhenFirstDayIsMondayAndDateIsThursday_ReturnsEndOfNextSunday()
+        {
+            DateTime testDate = new DateTime(2000, 1, 20);
+            DateTime expectedResult = new DateTime(2000, 1, 24).AddTicks(-1);
+
+            var result = testDate.EndOfWeek(true);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        public void EndOfWeek_WhenFirstDayIsMondayAndDateIsSunday_ReturnsEndOfSameDay()
+        {
+            DateTime testDate = new DateTime(2000, 1, 2);
+            DateTime expectedResult = new DateTime(2000, 1, 3).AddTicks(-1);
+
+            var result = testDate.EndOfWeek();
+
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
---
+++ Tests/MarkSFrancis.Tests/Extensions/Time/DateTimeExtensionsTests/StartOfWeekTests.cs
@@ -0,0 +1,73 @@
+﻿using MarkSFrancis.Extensions.Time;
+using NUnit.Framework;
+using System;
+
+namespace MarkSFrancis.Tests.Extensions.Time.DateTimeExtensionsTests
+{
+    public class StartOfWeekTests
+    {
+        [Test]
+        public void StartOfWeek_WhenFirstDayIsSundayAndDateIsSaturday_ReturnsPreviousSunday()
+        {
+            DateTime testDate = new DateTime(2000, 1, 1);
+            DateTime expectedResult = new DateTime(1999, 12, 26);
+
+            var result = testDate.StartOfWeek(false);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        public void StartOfWeek_WhenFirstDayIsSundayAndDateIsWednesday_ReturnsPreviousSunday()
+        {
+            DateTime testDate = new DateTime(2000, 1, 19);
+            DateTime expectedResult = new DateTime(2000, 1, 16);
+
+            var result = testDate.StartOfWeek(false);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        public void StartOfWeek_WhenFirstDayIsSundayAndDateIsSunday_ReturnsSameDate()
+        {
+            DateTime testDate = new DateTime(2000, 1, 2);
+
+            var result = testDate.StartOfWeek(false);
+
+            Assert.AreEqual(testDate, result);
+        }
+
+        [Test]
+        public void StartOfWeek_WhenFirstDayIsMondayAndDateIsSunday_ReturnsPreviousMonday()
+        {
+            DateTime testDate = new DateTime(2000, 1, 2);
+            DateTime expectedResult = new DateTime(1999, 12, 27);
+
+            var result = testDate.StartOfWeek();
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        public void StartOfWeek_WhenFirstDayIsMondayAndDateIsThursday_ReturnsPreviousMonday()
+        {
+            DateTime testDate = new DateTime(2000, 1, 20);
+            DateTime expectedResult = new DateTime(2000, 1, 17);
+
+            var result = testDate.StartOfWeek(true);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        public void StartOfWeek_WhenFirstDayIsMondayAndDateIsMonday_ReturnsSameDate()
+        {
+            DateTime testDate = new DateTime(2000, 1, 3);
+
+            var result = testDate.StartOfWeek();
+
+            Assert.AreEqual(testDate, result);
+        }
+    }
+}
