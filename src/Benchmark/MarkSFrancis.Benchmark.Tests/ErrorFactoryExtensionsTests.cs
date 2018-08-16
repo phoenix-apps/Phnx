0f0fdc9fa070667deefba4fcb44cd22493b3f18e
Mark Francis <mrkfrancis8@gmail.com>
2018-08-16 16:15:01 +0100

    Added tests for Benchmark -> ErrorFactory, BenchmarkSectionTime and BenchmarkSection

---
+++ src/Benchmark/MarkSFrancis.Benchmark.Tests/ErrorFactoryExtensionsTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace MarkSFrancis.Benchmark.Tests
+{
+    public class ErrorFactoryExtensionsTests
+    {
+        [Test]
+        public void ErrorFactory_GetBenchmarkNotFinished_ReturnsInvalidOperationException()
+        {
+            Assert.IsInstanceOf<InvalidOperationException>(ErrorFactory.Default.BenchmarkNotFinished());
+        }
+    }
+}
