2ed73795ef53d6634e4a163699414ba192793c8b
Mark Francis <mrkfrancis8@gmail.com>
2018-08-29 14:34:58 +0100

    FIxed a bug where RemoveBy would remove all items except the subsequent matches for removeBy when removeAllOccurences is false

--- src/Collections/MarkSFrancis.Collections/Extensions/IEnumerableLinqExtensions.cs
+++ src/Collections/MarkSFrancis.Collections/Extensions/IEnumerableLinqExtensions.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace MarkSFrancis.Collections.Extensions
 {
@@ -10,24 +9,36 @@ namespace MarkSFrancis.Collections.Extensions
     public static class IEnumerableLinqExtensions
     {
         /// <summary>Gets unique elements from a collection according to the key selector</summary>
-        /// <param name="values">The collection to get distinct elements from</param>
+        /// <param name="source">The collection to get distinct elements from</param>
         /// <param name="keySelector">A function to extract the key for each value</param>
         /// <typeparam name="TSource">The type of the elements of source</typeparam>
         /// <typeparam name="TKey">The type of the key returned by <paramref name="keySelector"/></typeparam>
         /// <returns>Unique elements by the <paramref name="keySelector"/></returns>
-        /// <exception cref="ArgumentNullException"><paramref name="values"/> or <paramref name="keySelector"/> is null</exception>
-        public static IEnumerable<TSource> DistinctBy<TSource, TKey>(this IEnumerable<TSource> values, Func<TSource, TKey> keySelector)
+        /// <exception cref="ArgumentNullException"><paramref name="source"/> or <paramref name="keySelector"/> is null</exception>
+        public static IEnumerable<TSource> DistinctBy<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
         {
-            if (values == null)
+            if (source == null)
             {
-                ErrorFactory.Default.ArgumentNull(nameof(values));
+                throw ErrorFactory.Default.ArgumentNull(nameof(source));
             }
             else if (keySelector == null)
             {
-                ErrorFactory.Default.ArgumentNull(nameof(keySelector));
+                throw ErrorFactory.Default.ArgumentNull(nameof(keySelector));
             }
 
-            return values.GroupBy(keySelector).Select(x => x.First());
+            HashSet<TKey> knownDistincts = new HashSet<TKey>();
+
+            foreach (var item in source)
+            {
+                var key = keySelector(item);
+                if (knownDistincts.Contains(key))
+                {
+                    continue;
+                }
+
+                yield return item;
+                knownDistincts.Add(key);
+            }
         }
 
         /// <summary>
@@ -35,16 +46,16 @@ namespace MarkSFrancis.Collections.Extensions
         /// </summary>
         /// <typeparam name="T">The type of values in the collection</typeparam>
         /// <typeparam name="TKey">The type of the key to use for comparison</typeparam>
-        /// <param name="enumerable">The collection of values to get the maximum value from</param>
+        /// <param name="source">The collection of values to get the maximum value from</param>
         /// <param name="keySelector">The selector to get the key to use for comparing values</param>
         /// <returns>The value from the collection with the maximum key value</returns>
-        /// <exception cref="ArgumentNullException"><paramref name="enumerable"/> is null or <paramref name="keySelector"/> is null</exception>
-        /// <exception cref="IndexOutOfRangeException"><paramref name="enumerable"/> was empty</exception>
-        public static T MaxBy<T, TKey>(this IEnumerable<T> enumerable, Func<T, TKey> keySelector) where TKey : IComparable<TKey>
+        /// <exception cref="ArgumentNullException"><paramref name="source"/> is null or <paramref name="keySelector"/> is null</exception>
+        /// <exception cref="ArgumentException"><paramref name="source"/> was empty</exception>
+        public static T MaxBy<T, TKey>(this IEnumerable<T> source, Func<T, TKey> keySelector) where TKey : IComparable<TKey>
         {
-            if (enumerable == null)
+            if (source == null)
             {
-                throw ErrorFactory.Default.ArgumentNull(nameof(enumerable));
+                throw ErrorFactory.Default.ArgumentNull(nameof(source));
             }
             else if (keySelector == null)
             {
@@ -55,7 +66,7 @@ namespace MarkSFrancis.Collections.Extensions
             TKey curMaxValue = default(TKey);
             bool firstRun = true;
 
-            foreach (var value in enumerable)
+            foreach (var value in source)
             {
                 if (firstRun)
                 {
@@ -76,7 +87,7 @@ namespace MarkSFrancis.Collections.Extensions
 
             if (firstRun)
             {
-                throw ErrorFactory.Default.CollectionEmpty(nameof(enumerable));
+                throw ErrorFactory.Default.ArgumentException("Collection was empty", nameof(source));
             }
 
             return curMax;
@@ -87,16 +98,16 @@ namespace MarkSFrancis.Collections.Extensions
         /// </summary>
         /// <typeparam name="T">The type of values in the collection</typeparam>
         /// <typeparam name="TKey">The type of the key to use for comparison</typeparam>
-        /// <param name="enumerable">The collection of values to get the minimum value from</param>
+        /// <param name="source">The collection of values to get the minimum value from</param>
         /// <param name="keySelector">The selector to get the key to use for comparing values</param>
         /// <returns>The value from the collection with the minimum key value</returns>
-        /// <exception cref="ArgumentNullException"><paramref name="enumerable"/> is null or <paramref name="keySelector"/> is null</exception>
-        /// <exception cref="IndexOutOfRangeException"><paramref name="enumerable"/> was empty</exception>
-        public static T MinBy<T, TKey>(this IEnumerable<T> enumerable, Func<T, TKey> keySelector) where TKey : IComparable<TKey>
+        /// <exception cref="ArgumentNullException"><paramref name="source"/> is null or <paramref name="keySelector"/> is null</exception>
+        /// <exception cref="ArgumentException"><paramref name="source"/> was empty</exception>
+        public static T MinBy<T, TKey>(this IEnumerable<T> source, Func<T, TKey> keySelector) where TKey : IComparable<TKey>
         {
-            if (enumerable == null)
+            if (source == null)
             {
-                throw ErrorFactory.Default.ArgumentNull(nameof(enumerable));
+                throw ErrorFactory.Default.ArgumentNull(nameof(source));
             }
             else if (keySelector == null)
             {
@@ -107,7 +118,7 @@ namespace MarkSFrancis.Collections.Extensions
             TKey curMinValue = default(TKey);
             bool firstRun = true;
 
-            foreach (var value in enumerable)
+            foreach (var value in source)
             {
                 if (firstRun)
                 {
@@ -128,7 +139,7 @@ namespace MarkSFrancis.Collections.Extensions
 
             if (firstRun)
             {
-                throw ErrorFactory.Default.CollectionEmpty(nameof(enumerable));
+                throw ErrorFactory.Default.ArgumentException("Collection was empty", nameof(source));
             }
 
             return curMin;
@@ -138,15 +149,15 @@ namespace MarkSFrancis.Collections.Extensions
         /// <summary>
         /// Removes the first occurrence of an object which matches a given query from the collection
         /// </summary>
-        /// <param name="enumerable">The collection</param>
+        /// <param name="source">The collection</param>
         /// <param name="removeBy">The selector for the object to remove from the collection</param>
         /// <param name="removeAllOccurences">Whether to remove all occurences, or just the first occurence</param>
         /// <returns>
         /// <see langword="true"/> if an entry is successfully removed; otherwise, <see langword="false"/>.  This method also returns <see langword="false"/> if no entries matching <paramref name="removeBy"/> were found in the collection</returns>
-        public static IEnumerable<T> RemoveBy<T>(this IEnumerable<T> enumerable, Func<T, bool> removeBy, bool removeAllOccurences = true)
+        public static IEnumerable<T> RemoveBy<T>(this IEnumerable<T> source, Func<T, bool> removeBy, bool removeAllOccurences = true)
         {
             bool foundFirst = false;
-            foreach (var entry in enumerable)
+            foreach (var entry in source)
             {
                 if (foundFirst)
                 {
@@ -161,6 +172,10 @@ namespace MarkSFrancis.Collections.Extensions
                         foundFirst = true;
                     }
                 }
+                else
+                {
+                    yield return entry;
+                }
             }
         }
 
@@ -168,19 +183,19 @@ namespace MarkSFrancis.Collections.Extensions
         /// Gets whether two collections contain the same data using <see cref="EqualityComparer{T}.Default"/>
         /// </summary>
         /// <typeparam name="T">The type of values to compare</typeparam>
-        /// <param name="enumerable">The first collection to compare</param>
+        /// <param name="source">The first collection to compare</param>
         /// <param name="rangeToCompare">The collection to compare with</param>
         /// <returns>Whether the two collections contain the same data. Returns <see langword="false"/> if either collection is <see langword="null"/></returns>
-        public static bool EqualsRange<T>(this IEnumerable<T> enumerable, IEnumerable<T> rangeToCompare)
+        public static bool EqualsRange<T>(this IEnumerable<T> source, IEnumerable<T> rangeToCompare)
         {
-            if (enumerable == null || rangeToCompare == null)
+            if (source == null || rangeToCompare == null)
             {
                 return false;
             }
 
             var comparer = EqualityComparer<T>.Default;
 
-            using (var enumerator1 = enumerable.GetEnumerator())
+            using (var enumerator1 = source.GetEnumerator())
             {
                 using (var enumerator2 = rangeToCompare.GetEnumerator())
                 {
@@ -209,16 +224,16 @@ namespace MarkSFrancis.Collections.Extensions
         }
 
         /// <summary>
-        /// Converts this collection to a <see cref="List{T}"/> with a given default capacity. Useful if you know how many values will be in <paramref name="enumerable"/>
+        /// Converts this collection to a <see cref="List{T}"/> with a given default capacity. Useful if you know how many values will be in <paramref name="source"/>
         /// </summary>
         /// <typeparam name="T">The type of values in the collection</typeparam>
-        /// <param name="enumerable">The collection of values</param>
+        /// <param name="source">The collection of values</param>
         /// <param name="capacity">The capacity to assign to the new <see cref="List{T}"/></param>
         /// <returns>A list of values as a copy of an <see cref="IEnumerable{T}"/></returns>
-        public static List<T> ToList<T>(this IEnumerable<T> enumerable, int capacity)
+        public static List<T> ToList<T>(this IEnumerable<T> source, int capacity)
         {
             var newList = new List<T>(capacity);
-            newList.AddRange(enumerable);
+            newList.AddRange(source);
 
             return newList;
         }
@@ -227,24 +242,24 @@ namespace MarkSFrancis.Collections.Extensions
         /// Append multiple collections of <see cref="IEnumerable{T}"/> onto a single collection
         /// </summary>
         /// <typeparam name="T">The type of values in the collections</typeparam>
-        /// <param name="collection">The collection to extend</param>
-        /// <param name="collections">The collections to extend onto the <paramref name="collection"/></param>
+        /// <param name="source">The collection to extend</param>
+        /// <param name="collections">The collections to extend onto the <paramref name="source"/></param>
         /// <returns>A collection containing all given collection's values</returns>
-        public static IEnumerable<T> Append<T>(this IEnumerable<T> collection, params IEnumerable<T>[] collections)
+        public static IEnumerable<T> Append<T>(this IEnumerable<T> source, params IEnumerable<T>[] collections)
         {
-            return collection.Append((IEnumerable<IEnumerable<T>>)collections);
+            return source.Append((IEnumerable<IEnumerable<T>>)collections);
         }
 
         /// <summary>
         /// Append multiple collections of <see cref="IEnumerable{T}"/> onto a single collection
         /// </summary>
         /// <typeparam name="T">The type of values in the collections</typeparam>
-        /// <param name="collection">The collection to extend</param>
-        /// <param name="collections">The collections to extend onto the <paramref name="collection"/></param>
+        /// <param name="source">The collection to extend</param>
+        /// <param name="collections">The collections to extend onto the <paramref name="source"/></param>
         /// <returns>A collection containing all given collection's values</returns>
-        public static IEnumerable<T> Append<T>(this IEnumerable<T> collection, IEnumerable<IEnumerable<T>> collections)
+        public static IEnumerable<T> Append<T>(this IEnumerable<T> source, IEnumerable<IEnumerable<T>> collections)
         {
-            foreach (var value in collection)
+            foreach (var value in source)
             {
                 yield return value;
             }
@@ -259,11 +274,11 @@ namespace MarkSFrancis.Collections.Extensions
         /// Flatten a two dimensional collection into a single one dimensional collection
         /// </summary>
         /// <typeparam name="T">The type of values in the collection</typeparam>
-        /// <param name="collection">The collection to flatten</param>
+        /// <param name="source">The collection to flatten</param>
         /// <returns>A one dimensional collection containing all the two dimensional collection's values</returns>
-        public static IEnumerable<T> Flatten<T>(this IEnumerable<IEnumerable<T>> collection)
+        public static IEnumerable<T> Flatten<T>(this IEnumerable<IEnumerable<T>> source)
         {
-            foreach (var subCollection in collection)
+            foreach (var subCollection in source)
             {
                 foreach (var value in subCollection)
                 {
@@ -276,13 +291,13 @@ namespace MarkSFrancis.Collections.Extensions
         /// Get the index of an item that matches a criteria
         /// </summary>
         /// <typeparam name="T">The type of values in the collection</typeparam>
-        /// <param name="collection">The collection to search</param>
+        /// <param name="source">The collection to search</param>
         /// <param name="isMatch">The criteria that the value must pass in order to be a match</param>
-        /// <returns>The index of the first occurance of an item within <paramref name="collection"/> that passes <paramref name="isMatch"/>. If the item is not found, this returns -1</returns>
-        public static int IndexOf<T>(this IEnumerable<T> collection, Func<T, bool> isMatch)
+        /// <returns>The index of the first occurance of an item within <paramref name="source"/> that passes <paramref name="isMatch"/>. If the item is not found, this returns -1</returns>
+        public static int IndexOf<T>(this IEnumerable<T> source, Func<T, bool> isMatch)
         {
             int index = 0;
-            foreach (var item in collection)
+            foreach (var item in source)
             {
                 if (isMatch(item))
                 {
@@ -300,13 +315,13 @@ namespace MarkSFrancis.Collections.Extensions
         /// Get the index of an item that matches a criteria
         /// </summary>
         /// <typeparam name="T">The type of values in the collection</typeparam>
-        /// <param name="collection">The collection to search</param>
+        /// <param name="source">The collection to search</param>
         /// <param name="isMatch">The criteria that the value must pass in order to be a match</param>
-        /// <returns>The index of the first occurance of an item within <paramref name="collection"/> that passes <paramref name="isMatch"/>. If the item is not found, this returns -1</returns>
-        public static long IndexOfLong<T>(this IEnumerable<T> collection, Func<T, bool> isMatch)
+        /// <returns>The index of the first occurance of an item within <paramref name="source"/> that passes <paramref name="isMatch"/>. If the item is not found, this returns -1</returns>
+        public static long IndexOfLong<T>(this IEnumerable<T> source, Func<T, bool> isMatch)
         {
             long index = 0;
-            foreach (var item in collection)
+            foreach (var item in source)
             {
                 if (isMatch(item))
                 {
