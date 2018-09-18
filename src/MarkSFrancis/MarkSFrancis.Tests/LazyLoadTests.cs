a108ac7d40cf05a0df8c93988c77591624a04932
Mark Francis <mrkfrancis8@gmail.com>
2018-09-18 11:03:58 +0100

    Added LazyLoad helper to enable implicitly getting a lazy loaded object's value (as T)

---
+++ src/MarkSFrancis/MarkSFrancis.Tests/LazyLoadTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace MarkSFrancis.Tests
+{
+    public class LazyLoadTests
+    {
+        [Test]
+        public void Converter_GetsValue()
+        {
+            const string expected = "asdf";
+            var lazy = new LazyLoad<string>(() => expected);
+
+            string result = lazy;
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
