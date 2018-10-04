787933b30101c50ccb8105790ed1af719649bda5
Mark Francis <mrkfrancis8@gmail.com>
2018-10-04 15:00:21 +0100

    Added ICollection support for SeedGroup and SeedGroupAsync

--- src/Data/Data/Phnx.Data/Seeds/SeedGroup.cs
+++ src/Data/Data/Phnx.Data/Seeds/SeedGroup.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace Phnx.Data.Seeds
@@ -6,16 +7,14 @@ namespace Phnx.Data.Seeds
     /// <summary>
     /// A group of seeds, used to help setup and organise seed operations for a database
     /// </summary>
-    public class SeedGroup : ISeed
+    public class SeedGroup : ISeed, ICollection<ISeed>
     {
         /// <summary>
-        /// Create a new seed group from a range of seeds
+        /// Create a new seed group with an empty range of seeds
         /// </summary>
-        /// <param name="seeds">The seed group to initalise from</param>
-        /// <exception cref="ArgumentNullException"><paramref name="seeds"/> is <see langword="null"/></exception>
-        public SeedGroup(params ISeed[] seeds)
+        public SeedGroup()
         {
-            _seeds = new List<ISeed>(seeds) ?? throw new ArgumentNullException(nameof(seeds));
+            _seeds = new List<ISeed>();
         }
 
         /// <summary>
@@ -23,12 +22,31 @@ namespace Phnx.Data.Seeds
         /// </summary>
         /// <param name="seeds">The seed group to initalise from</param>
         /// <exception cref="ArgumentNullException"><paramref name="seeds"/> is <see langword="null"/></exception>
-        public SeedGroup(IEnumerable<ISeed> seeds)
+        /// <exception cref="ArgumentException">One or more of the enties in <paramref name="seeds"/> is  <see langword="null"/></exception>
+        public SeedGroup(params ISeed[] seeds) : this()
         {
-            _seeds = new List<ISeed>(seeds) ?? throw new ArgumentNullException(nameof(seeds));
+            Add(seeds);
+        }
+
+        /// <summary>
+        /// Create a new seed group from a range of seeds
+        /// </summary>
+        /// <param name="seeds">The seed group to initalise from</param>
+        /// <exception cref="ArgumentNullException"><paramref name="seeds"/> is <see langword="null"/></exception>
+        /// <exception cref="ArgumentException">One or more of the enties in <paramref name="seeds"/> is  <see langword="null"/></exception>
+        public SeedGroup(IEnumerable<ISeed> seeds) : this()
+        {
+            Add(seeds);
         }
 
         private readonly List<ISeed> _seeds;
+
+        /// <summary>
+        /// The number of entries in this group
+        /// </summary>
+        public int Count => _seeds.Count;
+
+        bool ICollection<ISeed>.IsReadOnly => false;
 
         /// <summary>
         /// Add a single seed
@@ -47,6 +65,11 @@ namespace Phnx.Data.Seeds
             return this;
         }
 
+        void ICollection<ISeed>.Add(ISeed item)
+        {
+            Add(item);
+        }
+
         /// <summary>
         /// Add a range of seeds
         /// </summary>
@@ -60,7 +83,32 @@ namespace Phnx.Data.Seeds
                 throw new ArgumentNullException(nameof(seeds));
             }
 
-            _seeds.AddRange(seeds);
+            // Optimise list
+            if (_seeds.Capacity < _seeds.Count + seeds.Length)
+            {
+                int sizeShouldBe = _seeds.Capacity;
+                if (sizeShouldBe <= 0)
+                {
+                    sizeShouldBe = 4;
+                }
+
+                while (sizeShouldBe < _seeds.Count + seeds.Length)
+                {
+                    sizeShouldBe *= 2;
+                }
+
+                _seeds.Capacity = sizeShouldBe;
+            }
+
+            foreach (var seed in seeds)
+            {
+                if (seed is null)
+                {
+                    throw new ArgumentException($"Entry {_seeds.Count} cannot be null", nameof(seeds));
+                }
+                _seeds.Add(seed);
+            }
+
             return this;
         }
 
@@ -77,7 +125,15 @@ namespace Phnx.Data.Seeds
                 throw new ArgumentNullException(nameof(seeds));
             }
 
-            _seeds.AddRange(seeds);
+            foreach (var seed in seeds)
+            {
+                if (seed is null)
+                {
+                    throw new ArgumentException($"Entry {_seeds.Count} cannot be null", nameof(seeds));
+                }
+                _seeds.Add(seed);
+            }
+
             return this;
         }
 
@@ -88,13 +144,67 @@ namespace Phnx.Data.Seeds
         {
             for (int i = 0; i < _seeds.Count; i++)
             {
-                if (_seeds[i] is null)
-                {
-                    throw new NullReferenceException($"One or more seeds were null in this seed group");
-                }
-
                 _seeds[i].Run();
             }
+        }
+
+        /// <summary>
+        /// Removes all seeds
+        /// </summary>
+        public void Clear()
+        {
+            _seeds.Clear();
+        }
+
+        /// <summary>
+        /// Determines whether a seed is in this group
+        /// </summary>
+        /// <param name="item">The seed to locate</param>
+        /// <returns><see langword="true"/> if item is found in the group, otherwise <see langword="false"/></returns>
+        public bool Contains(ISeed item)
+        {
+            return _seeds.Contains(item);
+        }
+
+        /// <summary>
+        /// Copies the group to a compatible one dimensional array, starting at <paramref name="arrayIndex"/>
+        /// </summary>
+        /// <param name="array">The array to copy to</param>
+        /// <param name="arrayIndex">The zero-based index in array at which copying begins</param>
+        /// <exception cref="ArgumentNullException"><paramref name="array"/> is <see langword="null"/></exception>
+        /// <exception cref="ArgumentOutOfRangeException"><paramref name="arrayIndex"/> is less than zero</exception>
+        /// <exception cref="ArgumentException">The number of seeds in this group is greater than the available space from <paramref name="arrayIndex"/> to the end of <paramref name="array"/></exception>
+        public void CopyTo(ISeed[] array, int arrayIndex)
+        {
+            _seeds.CopyTo(array, arrayIndex);
+        }
+
+        /// <summary>
+        /// Removes the first occurrence of a specific seed from the group
+        /// </summary>
+        /// <param name="item">The seed to remove from the group</param>
+        /// <returns><see langword="true"/> if the seed is successfully removed, otherwise <see langword="false"/>. This method also returns <see langword="false"/> if the seed was not found in the group</returns>
+        public bool Remove(ISeed item)
+        {
+            return _seeds.Remove(item);
+        }
+
+        /// <summary>
+        /// Returns an enumerator that iterates through the group
+        /// </summary>
+        /// <returns>An <see cref="IEnumerator{ISeedAsync}"/> for the group</returns>
+        public IEnumerator<ISeed> GetEnumerator()
+        {
+            return _seeds.GetEnumerator();
+        }
+
+        /// <summary>
+        /// Returns an enumerator that iterates through the group
+        /// </summary>
+        /// <returns>An <see cref="IEnumerator"/> for the group</returns>
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _seeds.GetEnumerator();
         }
     }
 }
--- src/Data/Data/Phnx.Data/Seeds/SeedGroupAsync.cs
+++ src/Data/Data/Phnx.Data/Seeds/SeedGroupAsync.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -7,16 +8,26 @@ namespace Phnx.Data.Seeds
     /// <summary>
     /// A group of seeds, used to help setup and organise seed operations for a database
     /// </summary>
-    public class SeedGroupAsync : ISeedAsync
+    public class SeedGroupAsync : ISeedAsync, ICollection<ISeedAsync>
     {
+        /// <summary>
+        /// Create a new seed group with an empty range of seeds
+        /// </summary>
+        public SeedGroupAsync()
+        {
+            _seeds = new List<ISeedAsync>();
+        }
+
+
         /// <summary>
         /// Create a new seed group from a range of seeds
         /// </summary>
         /// <param name="seeds">The seed group to initalise from</param>
         /// <exception cref="ArgumentNullException"><paramref name="seeds"/> is <see langword="null"/></exception>
-        public SeedGroupAsync(params ISeedAsync[] seeds)
+        /// <exception cref="ArgumentException">One or more of the enties in <paramref name="seeds"/> is  <see langword="null"/></exception>
+        public SeedGroupAsync(params ISeedAsync[] seeds) : this()
         {
-            _seeds = new List<ISeedAsync>(seeds) ?? throw new ArgumentNullException(nameof(seeds));
+            Add(seeds);
         }
 
         /// <summary>
@@ -24,12 +35,20 @@ namespace Phnx.Data.Seeds
         /// </summary>
         /// <param name="seeds">The seed group to initalise from</param>
         /// <exception cref="ArgumentNullException"><paramref name="seeds"/> is <see langword="null"/></exception>
-        public SeedGroupAsync(IEnumerable<ISeedAsync> seeds)
+        /// <exception cref="ArgumentException">One or more of the enties in <paramref name="seeds"/> is  <see langword="null"/></exception>
+        public SeedGroupAsync(IEnumerable<ISeedAsync> seeds) : this()
         {
-            _seeds = new List<ISeedAsync>(seeds) ?? throw new ArgumentNullException(nameof(seeds));
+            Add(seeds);
         }
 
         private readonly List<ISeedAsync> _seeds;
+
+        /// <summary>
+        /// The number of entries in this group
+        /// </summary>
+        public int Count => _seeds.Count;
+
+        bool ICollection<ISeedAsync>.IsReadOnly => false;
 
         /// <summary>
         /// Add a single seed
@@ -48,6 +67,11 @@ namespace Phnx.Data.Seeds
             return this;
         }
 
+        void ICollection<ISeedAsync>.Add(ISeedAsync item)
+        {
+            Add(item);
+        }
+
         /// <summary>
         /// Add a range of seeds
         /// </summary>
@@ -61,7 +85,32 @@ namespace Phnx.Data.Seeds
                 throw new ArgumentNullException(nameof(seeds));
             }
 
-            _seeds.AddRange(seeds);
+            // Optimise list
+            if (_seeds.Capacity < _seeds.Count + seeds.Length)
+            {
+                int sizeShouldBe = _seeds.Capacity;
+                if (sizeShouldBe <= 0)
+                {
+                    sizeShouldBe = 4;
+                }
+
+                while (sizeShouldBe < _seeds.Count + seeds.Length)
+                {
+                    sizeShouldBe *= 2;
+                }
+
+                _seeds.Capacity = sizeShouldBe;
+            }
+
+            foreach (var seed in seeds)
+            {
+                if (seed is null)
+                {
+                    throw new ArgumentException($"Entry {_seeds.Count} cannot be null", nameof(seeds));
+                }
+                _seeds.Add(seed);
+            }
+
             return this;
         }
 
@@ -78,55 +127,105 @@ namespace Phnx.Data.Seeds
                 throw new ArgumentNullException(nameof(seeds));
             }
 
-            _seeds.AddRange(seeds);
+            foreach (var seed in seeds)
+            {
+                if (seed is null)
+                {
+                    throw new ArgumentException($"Entry {_seeds.Count} cannot be null", nameof(seeds));
+                }
+                _seeds.Add(seed);
+            }
+
             return this;
         }
 
         /// <summary>
-        /// Run all the <see cref="_seeds"/>
+        /// Run all the seeds
         /// </summary>
         /// <param name="runParallel">Whether to run the seeds in parallel (<see langword="true"/>) or series (<see langword="false"/>)</param>
-        public void RunSync(bool runParallel)
-        {
-            var task = RunAsync(runParallel);
-
-            task.Wait();
-        }
-
-        /// <summary>
-        /// Run all the <see cref="_seeds"/>
-        /// </summary>
-        /// <param name="runParallel">Whether to run the seeds in parallel (<see langword="true"/>) or series (<see langword="false"/>)</param>
-        public Task RunAsync(bool runParallel)
+        public async Task RunAsync(bool runParallel)
         {
             if (runParallel)
             {
-                return Task.Run(() => Parallel.ForEach(_seeds, seed => seed.RunAsync()));
+                await Task.Run(() => Parallel.ForEach(_seeds, async seed => await seed.RunAsync()));
             }
             else
             {
-                return Task.Run(() =>
+                await Task.Run(async () =>
                 {
                     foreach (var seed in _seeds)
                     {
-                        if (seed is null)
-                        {
-                            throw new NullReferenceException($"One or more seeds were null in this seed group");
-                        }
-
-                        var task = seed.RunAsync();
-                        task.Wait();
+                        await seed.RunAsync();
                     }
                 });
             }
         }
 
         /// <summary>
-        /// Run all the <see cref="_seeds"/> in series
+        /// Run all the seeds in series
         /// </summary>
         public Task RunAsync()
         {
             return RunAsync(false);
+        }
+
+        /// <summary>
+        /// Removes all seeds
+        /// </summary>
+        public void Clear()
+        {
+            _seeds.Clear();
+        }
+
+        /// <summary>
+        /// Determines whether a seed is in this group
+        /// </summary>
+        /// <param name="item">The seed to locate</param>
+        /// <returns><see langword="true"/> if item is found in the group, otherwise <see langword="false"/></returns>
+        public bool Contains(ISeedAsync item)
+        {
+            return _seeds.Contains(item);
+        }
+
+        /// <summary>
+        /// Copies the group to a compatible one dimensional array, starting at <paramref name="arrayIndex"/>
+        /// </summary>
+        /// <param name="array">The array to copy to</param>
+        /// <param name="arrayIndex">The zero-based index in array at which copying begins</param>
+        /// <exception cref="ArgumentNullException"><paramref name="array"/> is <see langword="null"/></exception>
+        /// <exception cref="ArgumentOutOfRangeException"><paramref name="arrayIndex"/> is less than zero</exception>
+        /// <exception cref="ArgumentException">The number of seeds in this group is greater than the available space from <paramref name="arrayIndex"/> to the end of <paramref name="array"/></exception>
+        public void CopyTo(ISeedAsync[] array, int arrayIndex)
+        {
+            _seeds.CopyTo(array, arrayIndex);
+        }
+
+        /// <summary>
+        /// Removes the first occurrence of a specific seed from the group
+        /// </summary>
+        /// <param name="item">The seed to remove from the group</param>
+        /// <returns><see langword="true"/> if the seed is successfully removed, otherwise <see langword="false"/>. This method also returns <see langword="false"/> if the seed was not found in the group</returns>
+        public bool Remove(ISeedAsync item)
+        {
+            return _seeds.Remove(item);
+        }
+
+        /// <summary>
+        /// Returns an enumerator that iterates through the group
+        /// </summary>
+        /// <returns>An <see cref="IEnumerator{ISeedAsync}"/> for the group</returns>
+        public IEnumerator<ISeedAsync> GetEnumerator()
+        {
+            return _seeds.GetEnumerator();
+        }
+
+        /// <summary>
+        /// Returns an enumerator that iterates through the group
+        /// </summary>
+        /// <returns>An <see cref="IEnumerator"/> for the group</returns>
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _seeds.GetEnumerator();
         }
     }
 }
