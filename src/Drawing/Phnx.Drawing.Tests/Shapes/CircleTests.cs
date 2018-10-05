f38b99b25d28df40e334d34575d4cdcf6b8b51cc
Mark Francis <mrkfrancis8@gmail.com>
2018-10-05 16:55:38 +0100

    Removed Circle's dependancy on Eclipse

---
+++ src/Drawing/Phnx.Drawing.Tests/Shapes/CircleTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using Phnx.Drawing.Shapes;
+using System.Linq;
+
+namespace Phnx.Drawing.Tests.Shapes
+{
+    public class CircleTests
+    {
+        [Test]
+        public void Circle_IsATypeOfShape()
+        {
+            var interfaces = typeof(Circle).GetInterfaces();
+            var isIShape = interfaces.Contains(typeof(IShape));
+            Assert.IsTrue(isIShape);
+        }
+    }
+}
--- src/Drawing/Phnx.Drawing/Shapes/Circle.cs
+++ src/Drawing/Phnx.Drawing/Shapes/Circle.cs
@@ -1,19 +1,37 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 
 namespace Phnx.Drawing.Shapes
 {
     /// <summary>
     /// A circle shape
     /// </summary>
-    public class Circle : Ellipse
+    public class Circle : IShape
     {
         /// <summary>
         /// Create a new circle
         /// </summary>
         /// <param name="centerPoint">The center point of the circle</param>
         /// <param name="radius">The radius of the circle</param>
-        public Circle(Point centerPoint, double radius) : base(centerPoint, radius, radius, 0)
+        public Circle(Point centerPoint, double radius)
         {
+            CenterPoint = centerPoint;
+            Radius = radius;
         }
+
+        /// <summary>
+        /// The center point of this <see cref="Circle"/>
+        /// </summary>
+        public Point CenterPoint { get; set; }
+
+        /// <summary>
+        /// The radius of this <see cref="Circle"/>
+        /// </summary>
+        public double Radius { get; set; }
+
+        /// <summary>
+        /// The area of this <see cref="Circle"/>
+        /// </summary>
+        public double Area => Radius * Radius * Math.PI;
     }
 }
