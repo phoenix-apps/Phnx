d4cf4b01e9f903c88d410607a8fd24528c695d6c
Mark Francis <mrkfrancis8@gmail.com>
2018-10-12 14:43:15 +0100

    Closes #23 - Added support for ordinals formatting

--- src/Phnx/Phnx.Tests/Extensions/Numeric/IntExtensionsTests.cs
+++ src/Phnx/Phnx.Tests/Extensions/Numeric/IntExtensionsTests.cs
@@ -91,5 +91,51 @@ namespace Phnx.Tests.Extensions.Numeric
 
             Assert.AreEqual("-00100", result);
         }
+
+        [Test]
+        public void ConvertingToOrdinal_WithNegativeNumber_GetsNegativeNumber()
+        {
+            var result = (-100).ToOrdinal();
+
+            Assert.AreEqual("-100", result);
+        }
+
+        [Test]
+        public void ConvertingToOrdinal_WithZero_GetsOrdinal()
+        {
+            var result = 0.ToOrdinal();
+
+            Assert.AreEqual("0th", result);
+        }
+
+        [Test]
+        public void ConvertingToOrdinal_With353_GetsOrdinal()
+        {
+            var result = 353.ToOrdinal();
+
+            Assert.AreEqual("353rd", result);
+        }
+
+        [Test]
+        public void ConvertingToOrdinal_With1Or2Or3Or4_GetsOrdinal()
+        {
+            var result1 = 1.ToOrdinal();
+            var result2 = 2.ToOrdinal();
+            var result3 = 3.ToOrdinal();
+            var result4 = 4.ToOrdinal();
+
+            Assert.AreEqual("1st", result1);
+            Assert.AreEqual("2nd", result2);
+            Assert.AreEqual("3rd", result3);
+            Assert.AreEqual("4th", result4);
+        }
+
+        [Test]
+        public void ConvertingToOrdinal_With13_GetsOrdinal()
+        {
+            var result = 13.ToOrdinal();
+
+            Assert.AreEqual("13th", result);
+        }
     }
 }
