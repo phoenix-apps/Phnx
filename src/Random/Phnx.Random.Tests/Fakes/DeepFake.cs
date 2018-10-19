954917bf72253c092c6f1faf8f00e0eb61d37d32
Mark Francis <mrkfrancis8@gmail.com>
2018-10-19 15:58:32 +0100

    Added tests for RandomT

---
+++ src/Random/Phnx.Random.Tests/Fakes/DeepFake.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Phnx.Random.Tests.Fakes
+{
+    public class DeepFake
+    {
+        public uint Id { get; set; }
+
+        public ShallowFake Single { get; set; }
+
+        public ShallowFake[] Collection { get; set; }
+
+        public RecursiveFake Recursive { get; set; }
+    }
+
+    public class ShallowFake
+    {
+        public string RandomText { get; set; }
+
+        public int VersionId { get; set; }
+
+        public Guid Id { get; set; }
+
+        public string IdAndVersionId => Id + ": " + VersionId;
+    }
+
+    public class RecursiveFake
+    {
+        public RecursiveFake Self { get; set; }
+    }
+}
---
+++ src/Random/Phnx.Random.Tests/GetRandomTTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using Phnx.Random.Generator;
+using Phnx.Random.Tests.Fakes;
+using System;
+
+namespace Phnx.Random.Tests
+{
+    public class GetRandomTTests
+    {
+        [Test]
+        public void GetT_Deep_PopulatesChildProperties()
+        {
+            var randomT = RandomTGenerator.Get<DeepFake>(false);
+
+            Assert.IsNotNull(randomT);
+            Assert.AreNotEqual(Guid.Empty, randomT.Id);
+            Assert.IsNotNull(randomT.Collection);
+            Assert.IsNotNull(randomT.Single);
+            Assert.IsNotNull(randomT.Recursive);
+        }
+
+        [Test]
+        public void GetT_Shallow_PopulatesOnlyCLRProperties()
+        {
+            var randomT = RandomTGenerator.Get<DeepFake>(true);
+
+            Assert.IsNotNull(randomT);
+            Assert.AreNotEqual(Guid.Empty, randomT.Id);
+            Assert.IsNull(randomT.Collection);
+            Assert.IsNull(randomT.Single);
+            Assert.IsNull(randomT.Recursive);
+        }
+    }
+}
