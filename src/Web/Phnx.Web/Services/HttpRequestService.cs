fac29ef5f3524be7e1812541f66af8a2d31049d9
Mark Francis <mrkfrancis8@gmail.com>
2018-11-27 17:01:14 +0000

    Improved null guards and documentation for HttpRequestService

--- src/Web/Phnx.Web/Services/HttpRequestService.cs
+++ src/Web/Phnx.Web/Services/HttpRequestService.cs
@@ -1,4 +1,5 @@
 ﻿using Phnx.Web.Fluent;
+using System;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -20,10 +21,11 @@ namespace Phnx.Web.Services
         /// <summary>
         /// Create a new <see cref="HttpRequestService"/> with a given <see cref="System.Net.Http.HttpClient"/>
         /// </summary>
-        /// <param name="httpClient"></param>
+        /// <param name="httpClient">The HTTP Client to use</param>
+        /// <exception cref="ArgumentNullException"><paramref name="httpClient"/> is <see langword="null"/></exception>
         public HttpRequestService(HttpClient httpClient)
         {
-            HttpClient = httpClient;
+            HttpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
         }
 
         /// <summary>
@@ -48,6 +50,9 @@ namespace Phnx.Web.Services
         /// </summary>
         /// <param name="request">The request to send</param>
         /// <returns>The response from the foreign URL</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="request"/> is <see langword="null"/></exception>
+        /// <exception cref="InvalidOperationException"><paramref name="request"/> has already been sent</exception>
+        /// <exception cref="HttpRequestException">The request failed due to an underlying issue such as network connectivity, DNS failure, server certification validation or timeout</exception>
         /// <remarks>This method will complete when only the response headers have been completely downloaded. Using <see cref="HttpContent.ReadAsStringAsync()"/> will wait until the content body is completely loaded</remarks>
         public virtual Task<HttpResponseMessage> SendAsync(HttpRequestMessage request)
         {
@@ -60,9 +65,21 @@ namespace Phnx.Web.Services
         /// <param name="request">The request to send</param>
         /// <param name="httpClient">The HTTP Client to use for the request</param>
         /// <returns>The response from the foreign URL</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="request"/> or <paramref name="httpClient"/> is <see langword="null"/></exception>
+        /// <exception cref="InvalidOperationException"><paramref name="request"/> has already been sent</exception>
+        /// <exception cref="HttpRequestException">The request failed due to an underlying issue such as network connectivity, DNS failure, server certification validation or timeout</exception>
         /// <remarks>This method will complete when only the response headers have been completely downloaded. Using <see cref="HttpContent.ReadAsStringAsync()"/> will wait until the content body is completely loaded</remarks>
         public virtual Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, HttpClient httpClient)
         {
+            if (request is null)
+            {
+                throw new ArgumentNullException(nameof(request));
+            }
+            if (httpClient is null)
+            {
+                throw new ArgumentNullException(nameof(httpClient));
+            }
+
             return httpClient.SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
         }
     }
