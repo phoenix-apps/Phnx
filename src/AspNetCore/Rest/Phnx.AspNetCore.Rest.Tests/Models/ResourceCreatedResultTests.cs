e7ccbac8c1cf6595534c36f732f8ae7a3e431b81
Mark Francis <mrkfrancis8@gmail.com>
2018-11-29 14:17:42 +0000

    Changed ResourceCreatedResult to a constructor pattern

---
+++ src/AspNetCore/Rest/Phnx.AspNetCore.Rest.Tests/Models/ResourceCreatedResultTests.cs
@@ -0,0 +1,64 @@
+﻿using NUnit.Framework;
+using Phnx.AspNet.Core.Rest.Models;
+
+namespace Phnx.AspNetCore.Rest.Tests.Models
+{
+    public class ResourceCreatedResultTests
+    {
+        [Test]
+        public void New_WithUrl_SetsByUrlToTrue()
+        {
+            var createdResult = new ResourceCreatedResult("test");
+
+            Assert.IsTrue(createdResult.ByUrl);
+        }
+
+        [Test]
+        public void New_WithUrl_SetsUrl()
+        {
+            string url = "test";
+            var createdResult = new ResourceCreatedResult(url);
+
+            Assert.AreEqual(url, createdResult.Url);
+        }
+
+        [Test]
+        public void New_WithActionControllerRoute_SetsByUrlToFalse()
+        {
+            var createdResult = new ResourceCreatedResult("test", "test", new object());
+
+            Assert.IsFalse(createdResult.ByUrl);
+        }
+
+        [Test]
+        public void New_WithNullActionControllerRoute_SetsByUrlToFalse()
+        {
+            var createdResult = new ResourceCreatedResult(null, null, null);
+
+            Assert.IsFalse(createdResult.ByUrl);
+        }
+
+        [Test]
+        public void New_WithActionControllerRoute_SetsValues()
+        {
+            string action = "act";
+            string controller = "cont";
+            object values = new
+            {
+                a = "b"
+            };
+
+            var createdResult = new ResourceCreatedResult(action, controller, values);
+
+            Assert.AreEqual(action, createdResult.ActionName);
+            Assert.AreEqual(controller, createdResult.ControllerName);
+            Assert.AreEqual(values, createdResult.RouteValues);
+        }
+
+        [Test]
+        public void New_WithNullActionControllerRoute_DoesNotThrow()
+        {
+            Assert.DoesNotThrow(() => new ResourceCreatedResult(null, null, null));
+        }
+    }
+}
--- src/AspNetCore/Rest/Phnx.AspNetCore.Rest/Models/ResourceCreatedResult.cs
+++ src/AspNetCore/Rest/Phnx.AspNetCore.Rest/Models/ResourceCreatedResult.cs
@@ -1,33 +1,18 @@
 ﻿namespace Phnx.AspNet.Core.Rest.Models
 {
+    /// <summary>
+    /// Captures information about the route/ URL at which a resource was created
+    /// </summary>
     public class ResourceCreatedResult
     {
-        private ResourceCreatedResult()
-        {
-        }
-
-        internal bool ByUrl { get; set; }
-
-        internal string Url { get; set; }
-
-        internal string ActionName { get; set; }
-
-        internal string ControllerName { get; set; }
-
-        internal object RouteValues { get; set; }
-
         /// <summary>
         /// The object was successfully created at the specified URL
         /// </summary>
         /// <param name="url">The URL at which the object was created</param>
-        /// <returns>A new <see cref="ResourceCreatedResult"/> that contains the URL at which the resource was created</returns>
-        public ResourceCreatedResult Created(string url)
+        public ResourceCreatedResult(string url)
         {
-            return new ResourceCreatedResult
-            {
-                Url = url,
-                ByUrl = true
-            };
+            ByUrl = true;
+            Url = url;
         }
 
         /// <summary>
@@ -36,16 +21,37 @@
         /// <param name="actionName">The name of the action for accessing the resource</param>
         /// <param name="controllerName">The name of the controller for accessing the resource</param>
         /// <param name="routeValues">The route values needed to access the resource</param>
-        /// <returns>A new <see cref="ResourceCreatedResult"/> that contains the URL at which the resource was created</returns>
-        public ResourceCreatedResult Created(string actionName, string controllerName, object routeValues)
+        public ResourceCreatedResult(string actionName, string controllerName, object routeValues)
         {
-            return new ResourceCreatedResult
-            {
-                ActionName = actionName,
-                ControllerName = controllerName,
-                RouteValues = routeValues,
-                ByUrl = false
-            };
+            ByUrl = false;
+            ActionName = actionName;
+            ControllerName = controllerName;
+            RouteValues = routeValues;
         }
+
+        /// <summary>
+        /// Whether the URL is tracked by an absolute <see cref="Url"/> (<see langword="true"/>), or by an <see cref="ActionName"/>, <see cref="ControllerName"/> and <see cref="RouteValues"/> combination (<see langword="false"/>)
+        /// </summary>
+        public bool ByUrl { get; }
+
+        /// <summary>
+        /// The absolute URL to the location at which the resource was created
+        /// </summary>
+        public string Url { get; }
+
+        /// <summary>
+        /// The name of the action at which the item was created
+        /// </summary>
+        public string ActionName { get; }
+
+        /// <summary>
+        /// The name of the controller at which the item was created
+        /// </summary>
+        public string ControllerName { get; }
+
+        /// <summary>
+        /// Route values for the route at which the item was created
+        /// </summary>
+        public object RouteValues { get; }
     }
 }
