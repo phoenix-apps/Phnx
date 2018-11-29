ee74ddd64d8749f3f1ca330fb1bc74cdf9acc1ab
Mark Francis <mrkfrancis8@gmail.com>
2018-11-29 13:52:46 +0000

    Added support for fluency in AddRest

--- src/AspNetCore/Rest/Phnx.AspNetCore.Rest/RestServiceBuilderExtensions.cs
+++ src/AspNetCore/Rest/Phnx.AspNetCore.Rest/RestServiceBuilderExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using Phnx.AspNetCore.Rest.Services;
 using Phnx.AspNetCore.Rest.Setup;
 
 namespace Phnx.AspNetCore.Rest
@@ -9,16 +10,18 @@ namespace Phnx.AspNetCore.Rest
     public static class RestServiceBuilderExtensions
     {
         /// <summary>
-        /// Setup all necessary components for <see cref="Rest"/>
+        /// Setup all necessary components for <see cref="Rest"/>. To use <see cref="RestControllerHelperService{TDataModel, TDtoModel, TPatchDtoModel}"/>, you'll need to inject your own implementation of <see cref="IResourceMapService{TDataModel, TDtoModel, TPatchDtoModel}"/> for the same types
         /// </summary>
         /// <param name="services">The service injector</param>
-        public static void AddRest(this IServiceCollection services)
+        public static IServiceCollection AddRest(this IServiceCollection services)
         {
             var builder = new RestServiceBuilder(services);
 
             builder.RegisterETags();
             builder.RegisterContextHelpers();
             builder.RegisterRestHelpers();
+
+            return services;
         }
     }
 }
