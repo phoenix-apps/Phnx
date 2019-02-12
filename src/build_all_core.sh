1d110293551179fa7beeed1319c47a9e9e72c58a
Mark Francis <mrkfrancis8@gmail.com>
2019-02-12 09:59:19 +0000

    Fixed build_all_core using wrong path to Phnx.Core.Configuration

--- src/build_all_core.sh
+++ src/build_all_core.sh
@@ -1,5 +1,5 @@
 # The list of projects to build. This can be a solution, .csproj, or a folder containing either. These are executed in order
-buildFiles=("Phnx" "Benchmark" "Collections" "Configuration" "Console" "Data/Data" "Data/EFCore" "Drawing" "IO/IO" "IO/Json" "IO/Threaded" "Reflection" "Security" "Serialization" "Web" "AspNetCore/AspNetCore" "AspNetCore/Modals" "AspNetCore/Rest")
+buildFiles=("Phnx" "Benchmark" "Collections" "Console" "Core/Configuration" "Data/Data" "Data/EFCore" "Drawing" "IO/IO" "IO/Json" "IO/Threaded" "Reflection" "Security" "Serialization" "Web" "AspNetCore/AspNetCore" "AspNetCore/Modals" "AspNetCore/Rest")
 
 buildStatus=0
 
