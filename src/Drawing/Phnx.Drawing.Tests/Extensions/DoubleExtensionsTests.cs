6a8bb6c60c643178d5bd0350da1f665155785eb2
Mark Francis <mrkfrancis8@gmail.com>
2019-02-13 14:29:19 +0000

    Added tests for DoubleExtensions

---
+++ src/Drawing/Phnx.Drawing.Tests/Extensions/DoubleExtensionsTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phnx.Drawing.Tests.Extensions
+{
+    public class DoubleExtensionsTests
+    {
+        [Test]
+        public void ToRadians_WithOneHundredEighty_ReturnsTwoPi()
+        {
+            var degrees = 180d;
+
+            var radians = degrees.ToRadians();
+
+            Assert.AreEqual(Math.PI, radians);
+        }
+
+        [Test]
+        public void ToDegrees_WithOnePointFivePi_ReturnsTwoHundredAndSeventy()
+        {
+            var radians = 1.5 * Math.PI;
+
+            var degrees = radians.ToDegrees();
+
+            Assert.AreEqual(270, degrees);
+        }
+    }
+}
