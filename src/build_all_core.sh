2b6856743d48927627bdbf63f0f6d8ab3d9a39fa
Mark Francis <mrkfrancis8@gmail.com>
2019-11-01 09:35:00 +0000

    Updated and fixed build all scripts

--- src/build_all_core.sh
+++ src/build_all_core.sh
@@ -1,5 +1,5 @@
 # The list of projects to build. This can be a solution, .csproj, or a folder containing either. These are executed in order
-buildFiles=("Phnx" "Benchmark" "Collections" "Console" "Core/Configuration" "Data/Data" "Data/EFCore" "Drawing" "IO/IO" "IO/Json" "IO/Threaded" "Reflection" "Security" "Serialization" "Web" "AspNetCore/AspNetCore" "AspNetCore/Modals" "AspNetCore/ETags")
+buildFiles=("Phnx" "Benchmark" "Collections" "Console" "Configuration" "Data/Data" "Data/EFCore" "Drawing" "IO/IO" "IO/Json" "IO/Threaded" "Reflection" "Security" "Serialization" "Web" "AspNetCore/Modals" "AspNetCore/ETags")
 
 buildStatus=0
 
